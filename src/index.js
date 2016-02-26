48df57fbefb67cf9451392e339c79643c4c88cd9
Laurent De Smet and Tim De Pauw <pair+laurent.desmet+tim.depauw@zentrick.com>
2016-02-26 16:28:30 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import listNpmDependencies from 'list-npm-dependencies'
+import slash from 'slash'
+
+const mapPackageDependencies = (dependencies, packages, map) => {
+  for (const dep of Object.keys(dependencies)) {
+    const version = dependencies[dep]
+    map[dep] = slash(packages[`${dep}@${version}`].path)
+  }
+}
+
+export default async (pkgRoot, options) => {
+  const { root, packages } = await listNpmDependencies(pkgRoot, options)
+
+  const config = {
+    map: {},
+    packages: {}
+  }
+
+  mapPackageDependencies(root.dependencies, packages, config.map)
+
+  for (const nameVersion of Object.keys(packages)) {
+    const pkgMeta = packages[nameVersion]
+    const map = {}
+
+    mapPackageDependencies(pkgMeta.dependencies, packages, map)
+
+    config.packages[slash(pkgMeta.path)] = {
+      main: pkgMeta.main,
+      format: pkgMeta.format,
+      map
+    }
+  }
+
+  return config
+}
